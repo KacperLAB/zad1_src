1ac678ba20e45bbe110e10f6bb2d9fb8d77d4996
KacperLAB <83961042+KacperLAB@users.noreply.github.com>
2023-05-12 16:48:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const hostname = 0.0.0.0;
+const hostname = '0.0.0.0';
 const port = 80;
 const server_start = new Date().toLocaleString();
 const server = http.createServer(function (req, res) {
