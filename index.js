20230df87d2bcdb125e331b439e506075ba9123f
KacperLAB <83961042+KacperLAB@users.noreply.github.com>
2023-05-12 17:00:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const port = 8000;
 const server_start = new Date().toLocaleString();
 const server = http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  const ipAddress = req.socket.remoteAddress;
+  var ipAddress = req.headers['x-forwarded-for'] ||
+     req.socket.remoteAddress ||
+     null;
+  //var ipAddress = req.socket.remoteAddress;
   var time = new Date();
   res.end(`IP: ${ipAddress}\n${time}`);
 });
-server.listen(port, hostname, function () {
+server.listen(port, hostname, server_start, function () {
   console.log(`Server running at http://${hostname}:${port}/`);
   console.log(`Uruchomiono: ${server_start}`)
   console.log("Autor: Kacper Papiński");
