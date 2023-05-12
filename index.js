bd7f2b7bc950b8cc1aeba7b16a9975937ef53606
KacperLAB <83961042+KacperLAB@users.noreply.github.com>
2023-05-12 17:28:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server = http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" });
   var ipAddress = req.socket.remoteAddress;
   var time = new Date();
-  res.end(`IP: ${ipAddress}\n${time}`);
+  res.end(`IP: ${ip.address()}\n${time}`);
 });
 server.listen(port, hostname, server_start, function () {
   console.log(`Server running at http://${hostname}:${port}/`);
