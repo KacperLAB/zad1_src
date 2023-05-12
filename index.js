9b5ffd9e1c92ae4cd85b9bb3a6c61ac3d6afbb37
KacperLAB <83961042+KacperLAB@users.noreply.github.com>
2023-05-12 17:12:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ const http = require("http");
 const hostname = '0.0.0.0';
 const port = 8000;
 const server_start = new Date().toLocaleString();
+var ip = require('ip');
 const server = http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  var ipAddress = req.headers['x-forwarded-for'] ||
-     req.socket.remoteAddress ||
-     null;
   //var ipAddress = req.socket.remoteAddress;
   var time = new Date();
-  res.end(`IP: ${ipAddress}\n${time}`);
+  res.end(`IP: ${ip.address()}\n${time}`);
 });
 server.listen(port, hostname, server_start, function () {
   console.log(`Server running at http://${hostname}:${port}/`);
