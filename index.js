0833f24b4c40866d8346b8f559ed8db012878697
KacperLAB <83961042+KacperLAB@users.noreply.github.com>
2023-05-12 15:17:21 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+// Constants
+const PORT = 8080;
+const HOST = 'localhost';
+const server_start = new Date().toLocaleString();
+// App
+const app = express();
+app.get('/', (req, res) => {
+  const ipAddress = req.socket.remoteAddress;
+  var time = new Date();
+  res.send(`IP: ${ipAddress}  <br> ${time}`)
+});
+
+app.listen(PORT, HOST, server_start, () => {
+  console.log(`Uruchomiono: ${server_start}`)
+  console.log("Autor: Kacper Papiński");
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
