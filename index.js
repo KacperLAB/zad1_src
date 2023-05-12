506e46c5c8bbd5b2a21e37cfb5f8bd3f2bfe5ebe
KacperLAB <83961042+KacperLAB@users.noreply.github.com>
2023-05-12 16:22:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const hostname = "localhost";
-const port = 8000;
+const port = 80;
 const server_start = new Date().toLocaleString();
 const server = http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" });
